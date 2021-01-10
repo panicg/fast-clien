28565848213f805fd49cded0259ed95b54b43caa
Seungman Kim <panicmg@gmail.com>
2021-01-10 19:12:02 +0900

    1. 파싱작업 준비

--- app/src/main/java/com/panicdev/fast_clien/common/ParsingController.kt
+++ app/src/main/java/com/panicdev/fast_clien/common/ParsingController.kt
@@ -1,4 +1,12 @@
 package com.panicdev.fast_clien.common
 
-class ParsingController {
+class ParsingController(private val board : MainBoard) {
+
+    val divider = "&=T31"
+
+
+    fun getList(pageNo : Int = 0, searchKeyword : String? = null){
+        val paramPageNo = "&po=$pageNo"
+        val callUrl = "${board.url}$divider$paramPageNo"
+    }
 }
